98c3cb0c9783dbaad0288861fe98b6e35397f2b6
Zephyr Tao <taozephyr@gmail.com>
2021-10-23 14:28:50 -0700

    timers

--- docs/timer.js
+++ docs/timer.js
@@ -1,9 +1,11 @@
 const startingMinutes20 = 0.2;
+const startingMinutes50 = 0.4;
 let time20 = startingMinutes20 * 60;
 
 const countdownEl = document.getElementById('20-min-countdown');
 
 function updateCountdown20() {
+    Boolean time = true;
     const minutes = Math.floor(time20 / 60);
     let seconds = time20 % 60;
 
@@ -15,7 +17,12 @@ function updateCountdown20() {
     time20 = time20 < 0 ? 0 : time20;
 
     if(time20 == 0) {
-        time20 = startingMinutes20 * 60;
+        if(time){
+            time20 = startingMinutes20 * 60;
+        } else{
+            time20 = startingMinutes50 * 60;
+        }
+
     }
 }
 
