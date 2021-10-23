40a6a36e077328131f830a8c113e30a43e4f9cd9
Zephyr Tao <taozephyr@gmail.com>
2021-10-23 14:24:12 -0700

    timers

--- docs/timer.js
+++ docs/timer.js
@@ -1,23 +1,44 @@
-const startingMinutes = 0.2;
-let time = startingMinutes * 60;
+const startingMinutes20 = 0.2;
+const startingMinutes50 = 0.4;
+let time20 = startingMinutes20 * 60;
+let time50 = startingMinutes50 * 60;
 
 const countdownEl = document.getElementById('20-min-countdown');
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+function updateCountdown20() {
+    const minutes = Math.floor(time20 / 60);
+    let seconds = time20 % 60;
 
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes}:${seconds}`;
-    time--;
+    time20--;
 
-    time = time < 0 ? 0 : time;
+    time20 = time20 < 0 ? 0 : time20;
 
-    if(time == 0) {
-        time = startingMinutes * 60;
+    if(time20 == 0) {
+        time20 = startingMinutes20 * 60;
     }
 }
 
-setInterval(updateCountdown, 1000);
-updateCountdown();
+setInterval(updateCountdown20, 1000);
+updateCountdown20();
+
+function updateCountdown50() {
+    const minutes = Math.floor(time50 / 60);
+    let seconds = time50 % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time50--;
+
+    time50 = time50 < 0 ? 0 : time50;
+
+    if(time50 == 0) {
+        time50 = startingMinutes50 * 60;
+    }
+}
+
+setInterval(updateCountdown50, 1000);
+updateCountdown50();
--- docs/timer2.js
+++ docs/timer2.js
@@ -1,9 +1,7 @@
-const startingMinutes = 50;
+const startingMinutes = 0.2;
 let time = startingMinutes * 60;
 
-const countdownEl = document.getElementById('50-min-countdown');
-
-setInterval(updateCountdown, 1000);
+const countdownEl = document.getElementById('20-min-countdown');
 
 function updateCountdown() {
     const minutes = Math.floor(time / 60);
@@ -13,4 +11,13 @@ function updateCountdown() {
 
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
+
+    time = time < 0 ? 0 : time;
+
+    if(time == 0) {
+        time = startingMinutes * 60;
+    }
 }
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
